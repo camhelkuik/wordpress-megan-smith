71eb60f5b8b9e3335aec8566fbe7ec4d48c6a6fb
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-09-13 13:36:42 -0500

    Footer looks more accurate

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
  */
 
 ?>
+<footer>
         <li>1111 N. 13th St, Suite 430, Omaha, NE 68102</li>
         <li>Email: megan@meagan.com</li>
         <li>Phone: 402.871.5920</li>
@@ -11,6 +12,8 @@
       
       <span class="footer-left"></span>
       <span class="footer-right"></span>
+    </footer>
+      
       <script type="text/javascript" src="global.js"></script>
 
   <?php wp_footer(); ?>
