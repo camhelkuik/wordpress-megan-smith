d60f7d0885c7784f555d2423f71ff5c1aa2932e3
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-09-25 10:07:07 -0500

    Erasing unnecessary files added to header.php

--- header.php
+++ header.php
@@ -1,25 +1,44 @@
+<?php
+/**
+ * The header for our theme.
+ */
+
+?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( ' | ', true, 'right' ); ?></title>
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-<div id="site-title"><?php if ( ! is_singular() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( ! is_singular() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</section>
-<nav id="menu" role="navigation">
-<div id="search">
-<?php get_search_form(); ?>
-</div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
-</header>
-<div id="container">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" type="text/css">
+
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  
+  <div class="arrow-down"></div>
+  <div class="main-content">  
+  
+    <div class="title">
+      <h1>Megan Smith Psychotherapy, PC</h1>
+      <img class="border" src="/assets/divider12.png"> 
+    </div>
+
+    <nav class="nav">
+      <?php wp_nav_menu(array(
+      'theme_location'  => 'primary',
+      'container'       => false,
+      'menu_id'         => 'menu',
+      'before'          => '',
+      'after'           => '',
+      'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s<div class="clear"></div></ul>',
+      'depth'           => 0,
+      'walker'          => ''
+    )); ?>
+    
+    <span class="corner-left"></span>
+    <span class="corner-right"></span>
+	</nav>
