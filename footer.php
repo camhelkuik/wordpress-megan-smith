49e548dd10e2f2e111c5bc612ed58ac094946f2f
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-09-25 12:24:46 -0500

    Minor adjustments to client info page and contact, added correct email to footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <footer id="footer" role="contentinfo">
 <div>
         <li>1111 N. 13th St, Suite 430, Omaha, NE 68102</li>
-        <li>Email: megan@meagan.com</li>
+        <li>Email: megsmith23@hotmail.com</li>
         <li>Phone: 402.871.5920</li>
 </div>
       <span class="footer-left"></span>
