58c60015f44967bb6cf5666482073d0a679dae65
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-10-01 12:13:58 -0500

    Figured out a fix for the menu, now need to adjust the css

--- header.php
+++ header.php
@@ -27,18 +27,18 @@
       <img class="border" src="http://localhost:8888/MeganSmith/wordpress-megan-smith/wp-content/uploads/2015/09/text-divider-diamond.png"> 
     </div>
 
-    <nav class="nav">
-      <?php wp_nav_menu(array(
-      'theme_location'  => 'primary',
-      'container'       => false,
-      'menu_id'         => 'menu',
-      'before'          => '',
-      'after'           => '',
-      'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s<div class="clear"></div></ul>',
-      'depth'           => 0,
-      'walker'          => ''
-    )); ?>
-    
+    <nav  id="menu" role="navigation" class="nav">
+  
+<?php wp_nav_menu(array(
+	'menu' => 'Main Menu', 
+	'container_id' => 'cssmenu', 
+	'walker' => new CSS_Menu_Walker()
+)); ?>
+     
     <span class="corner-left"></span>
     <span class="corner-right"></span>
 	</nav>
+ 
+
+  
+  
